a57a76541ef99f525d974ff00cfacecbf52b5b5c
Jon Daniel Palmares <jdpalmares@gmail.com>
2023-10-02 20:27:08 -0400

    Added 530 minimum absolute difference BST with java solution

---
+++ src/530-minimum-absolute-diff-bst/java/MinAbsDiffBST.java
@@ -0,0 +1,31 @@
+
+public class MinAbsDiffBST {
+    int prev = Integer.MAX_VALUE;
+    int ans = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+        inOrder(root);
+        return ans;
+    }
+
+    public void inOrder(TreeNode node){
+        if(node.left!=null)
+            inOrder(node.left);
+        ans = Math.min(ans,Math.abs(node.val-prev));
+        prev = node.val;
+        if(node.right!=null)
+            inOrder(node.right);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
