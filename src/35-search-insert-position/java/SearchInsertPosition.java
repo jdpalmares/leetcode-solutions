ccececd05ffedd002d6624ae9073c74d7c1fde69
Jon Daniel Palmares <jdpalmares@gmail.com>
2023-10-14 00:02:57 -0400

    Added 35 search insert position with Java Solution

---
+++ src/35-search-insert-position/java/SearchInsertPosition.java
@@ -0,0 +1,16 @@
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
+		
+		int left = 0, right = nums.length - 1;
+		while(left < right){
+			int mid = left + (right - left)/2;
+			
+			if(nums[mid] == target) return mid;
+			else if(nums[mid] > target) right = mid;
+			else left = mid + 1;
+		}
+		return nums[left] < target ? left + 1: left;
+    }
+}
