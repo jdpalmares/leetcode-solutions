00cb1c3737594ec975b9e9f50c96b5c94b518226
Jon Daniel Palmares <jdpalmares@gmail.com>
2023-11-05 01:38:42 -0400

    Fix java solution for 215 kth largest element

--- src/215-kth-largest-element-in-an-array/java/KthLargestElement.java
+++ src/215-kth-largest-element-in-an-array/java/KthLargestElement.java
@@ -1,24 +1,70 @@
+import java.util.PriorityQueue;
 
 public class KthLargestElement {
+    //solution using min-heap
     public int findKthLargest(int[] nums, int k) {
-        k = nums.length - k;
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int i = left;
-            for (int j = left + 1; j <= right; j++)
-                if (nums[j] < nums[left]) swap(nums, j, ++i);
-            swap(nums, left, i);
-
-            if (k < i) right = i - 1;
-            else if (k > i) left = i + 1;
-            else return nums[i];
+        PriorityQueue<Integer> ans = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) ans.offer(nums[i]);
+        
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > ans.peek()) {
+                ans.poll();
+                ans.offer(nums[i]);
+            }
         }
-        return -1;
+        
+        return ans.peek();
     }
 
-    private void swap(int[] nums, int x, int y) {
-        int temp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = temp;
-    }
+    //solution using quickSelect START
+    // public int findKthLargest(int[] nums, int k) {
+    //     return quickSelect(nums, 0, nums.length - 1, k - 1);
+    // }
+
+    // private int quickSelect(int[] nums, int left, int right, int k) {
+    //     while(true) {
+    //         if(left == right)
+    //             return nums[left];
+    //         int pivotIndex = getPivot(nums, left, right);
+    //         pivotIndex = partition(nums, left, right, pivotIndex);
+    //         if(pivotIndex == k)
+    //             return nums[k];
+    //         else if(pivotIndex > k)
+    //             right = pivotIndex-1;
+    //         else
+    //             left = pivotIndex+1;
+    //     }
+    // }
+
+    // private int getPivot(int[] nums, int left, int right) {
+    //     int mid = left + (right - left) / 2;
+    //     if(nums[right] > nums[left])
+    //         swap(nums, left, right);
+    //     if(nums[right] > nums[mid])
+    //         swap(nums, right, mid);
+    //     if(nums[mid] > nums[left])
+    //         swap(nums,left, mid);
+    //     return mid;
+    // }
+
+    // private int partition(int[] nums, int left, int right, int pivotIndex) {
+    //     int pivotValue = nums[pivotIndex];
+    //     swap(nums, pivotIndex, right);
+    //     int index = left;
+    //     for(int i = left; i < right; ++i) {
+    //         if(nums[i] > pivotValue) {
+    //             swap(nums, index, i);
+    //             ++index;
+    //         }
+    //     }
+    //     swap(nums, right, index);
+    //     return index;
+    // }
+
+    // private void swap(int[] nums, int x, int y) {
+    //     int temp = nums[x];
+    //     nums[x] = nums[y];
+    //     nums[y] = temp;
+    // }
+    //solution using quickSelect END
 }
